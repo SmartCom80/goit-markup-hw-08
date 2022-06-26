aca8696c8da4b18721be1b03d4cae8a884e12f20
Serhii Yehorov <smartcom.ter@gmail.com>
2022-06-26 23:21:01 +0300

    fixed mistakes

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-open-modal]"),
     closeModalBtn: document.querySelector("[data-close-modal]"),
     backdrop: document.querySelector("[data-backdrop]"),
+    body: document.querySelector("body")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.backdrop.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll")
   }
 })();
